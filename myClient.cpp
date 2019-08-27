69ac7dca81dc49711c8dc3483da96a340fcef702
Geraldine Eleazar <geleazar@uci.edu>
2019-08-27 12:14:36 -0700

    created main and imported iostream

---
+++ myClient.cpp
@@ -0,0 +1,9 @@
+//Code for a simple iterative client
+//geleazar v1.0
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	return 0;
+}
---
+++ myServer.cpp
@@ -0,0 +1,9 @@
+//Code for simple iterative server
+//geleazar v1.0
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	return 0;
+}
