fb0200a7b69f21a24f2dbfaa2ff8bc90b401a1cf
Geraldine Eleazar <geleazar@uci.edu>
2019-08-27 19:39:30 -0700

    updated client/server to get simple messages

--- myClient.cpp
+++ myClient.cpp
@@ -1,9 +1,46 @@
 //Code for a simple iterative client
 //geleazar v1.0
 #include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define PORT 800813
 using namespace std;
 
 int main(){
 
+	int newSock = 0, n = 0;
+	char data[1024];
+	struct sockaddr_in serverIP;
+	
+	memset(data, '0', sizeof(data));
+	
+	if((newSock = socket(AF_INET, SOCK_STREAM,0)) < 0)
+	{
+		cout << "Socket not created \n";
+		return 1;
+	}
+	
+	serverIP.sin_family = AF_INET;
+	serverIP.sin_port = htons(PORT);
+	serverIP.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	if(connect(newSock, (struct sockaddr *) &serverIP, sizeof(serverIP)) < 0)
+	{
+		cout << "Connection failed\n";
+		return 1;
+	}  
+	
+	while((n = read(newSock, data, sizeof(data) - 1)) > 0)
+	{
+		data[n] = 0;
+		cout << data << "\n";
+	}
+	close(newSock);
 	return 0;
 }
--- myServer.cpp
+++ myServer.cpp
@@ -1,9 +1,44 @@
 //Code for simple iterative server
 //geleazar v1.0
 #include <iostream>
+#include <cstring>
+#include <string>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 800813
+#define MAXSIZ 1048576
 using namespace std;
 
-int main(){
 
+
+int main(int argc, char *argv[])
+{
+	char data[1025]; //packet
+	int listenSock, connectSock, len, n;
+	struct sockaddr_in serverAddr;
+	
+	listenSock = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	memset(data, '0', sizeof(data));
+
+	serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(PORT);
+        serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	bind(listenSock, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	listen(listenSock, 5);
+
+	while(1)
+	{
+		cout << "Initializing...\n";
+		connectSock = accept(listenSock, NULL, NULL);
+		snprintf(data, sizeof(data), "hewwo\n");
+		write(connectSock, data, strlen(data));
+		close(connectSock);
+	}
 	return 0;
 }
